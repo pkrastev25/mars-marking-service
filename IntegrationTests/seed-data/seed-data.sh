3cf752d51f96f6707cacae2b07cabebcbe80d737
Petar <petar.krastev@haw-hamburg.de>
2018-05-17 22:14:50 +0300

    [FIX] Correct docker image host

--- IntegrationTests/seed-data/seed-data.sh
+++ IntegrationTests/seed-data/seed-data.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # metadata-svc
-mongoimport --host mongo --port 27017 --db import --collection metadata --type json --file ./metadata.json --jsonArray
+mongoimport --host mongodb --port 27017 --db import --collection metadata --type json --file ./metadata.json --jsonArray

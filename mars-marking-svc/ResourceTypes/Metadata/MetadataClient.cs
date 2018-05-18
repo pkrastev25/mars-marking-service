91e77f3c7c40e47f5f98b96410972a2f2107a168
Petar <petar.krastev@haw-hamburg.de>
2018-05-18 14:04:55 +0300

    [CHANGE] Metadata host

--- mars-marking-svc/ResourceTypes/Metadata/MetadataClient.cs
+++ mars-marking-svc/ResourceTypes/Metadata/MetadataClient.cs
@@ -28,7 +28,7 @@ namespace mars_marking_svc.ResourceTypes.Metadata
         )
         {
             var response = await _httpService.GetAsync(
-                $"http://nexus.informatik.haw-hamburg.de-metadata-svc/metadata/{metadataId}"
+                $"http://localhost:8080/metadata/{metadataId}"
             );
 
             response.ThrowExceptionIfNotSuccessfulResponse(
